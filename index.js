308d7951b988d3abc0f38bb7ed85ea2b26d96430
Samuel Holmez <samuelholmez@Samuels-MacBook-Air.local>
2023-06-19 16:44:42 -0400

    Finished CRUD

--- index.js
+++ index.js
@@ -35,16 +35,47 @@ const db = getFirestore();
 
 //---------------------------------------------------------------------------
 
-///now that we have some data, lets READ them
-db.collection('clothing').get()
-    .then(collection => {
-        // const clothing = collection.docs.map(doc =>{     |\
-        //     let item = doc.data()                        | \
-        //     item.id = doc.id                             |==> another way of writing whats below
-        //     return item                                  | /
-        // })                                               |/
-        const clothing = collection.docs.map(doc => ({...doc.data(), id: doc.id}))
-        console.table(clothing)
-    }) 
+// ///now that we have some data, lets READ them
+// db.collection('clothing').get()
+//     .then(collection => {
+//         // const clothing = collection.docs.map(doc =>{     |\
+//         //     let item = doc.data()                        | \
+//         //     item.id = doc.id                             |==> another way of writing whats below
+//         //     return item                                  | /
+//         // })                                               |/
+//         const clothing = collection.docs.map(doc => ({ ...doc.data(), id: doc.id }))
+//         console.table(clothing)
+//     })
+
+//     .catch(console.error);
+
+// //////////////////////////////////////////
     
-    .catch(console.error);
+// //lets say we want to find all of the items costing more than $100
+// db.collection('clothing').where("price", '>=', 100).get()
+//     .then(collection => {
+//         const clothing = collection.docs.map(doc => ({ ...doc.data(), id: doc.id }))
+//         console.table(clothing);
+//     })
+//     .catch(console.error)
+
+// /////////////////////////////////////////
+
+// ///now lets get a single doc by id (and use await to show)
+// const doc = await db.collection('clothing').doc('cruK0PKp16SnOkIWz32r').get()
+//         .catch(console.error);
+// const clothingItem = {...doc.data(), id: doc.id}
+// console.table(clothingItem);
+
+    //--------------------------------------
+
+//lets UPDATE a single document:
+db.collection('clothing').doc('cruK0PKp16SnOkIWz32r').update({ price: 120, qualityRating: 5.0 })
+    .then(doc =>  console.table("updated doc. "))
+    .catch(console.error)
+//--------------------------------------------------
+
+///even tho we SELDOM DELETE, here's how:
+
+await db.collection('clothing').doc('8fgNoNEcokJpgOItpicn').delete()
+    console.log("deleted")
